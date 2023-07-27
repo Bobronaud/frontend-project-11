a3e9ba1a6a794bf8d17490755f600f16fd66f50a
root <root@DESKTOP-AEVP059>
2023-07-27 19:47:17 +0300

    make requestToRss func for fix maintability in CodeClimate

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import uniqueId from 'lodash/uniqueId.js';
 import ru from './locales/ru.js';
 import render from './render.js';
 import parser from './parser.js';
-import checkNewPosts from './checkNewPosts.js';
 
 export default () => {
   const elements = {
@@ -62,69 +61,78 @@ export default () => {
           .validate({ link: elements.input.value })
           .then(() => {
             const rssLink = elements.input.value;
-            axios
-              .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(rssLink)}`)
-              .then((response) => {
-                const content = response.data.contents;
-                const xmlDom = parser(content);
-                const hasParserError = xmlDom.querySelector('parsererror');
-                if (hasParserError) {
-                  throw new Error('form.errors.failParsing');
-                }
-                return xmlDom;
-              })
-              .then((xmlDom) => {
-                const feedTitle = xmlDom.querySelector('title').textContent;
-                const feedDescription = xmlDom.querySelector('description').textContent;
-                const feedId = uniqueId();
-                watcher.feeds.push({
-                  title: feedTitle,
-                  description: feedDescription,
-                  id: feedId,
-                });
+            const requestToRss = (rss) => {
+              axios
+                .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(rss)}`)
+                .then((response) => {
+                  const content = response.data.contents;
+                  const xmlDom = parser(content);
+                  const hasParserError = xmlDom.querySelector('parsererror');
+                  if (hasParserError) {
+                    throw new Error('form.errors.failParsing');
+                  }
+                  const feedTitle = xmlDom.querySelector('title').textContent;
+                  const feedDescription = xmlDom.querySelector('description').textContent;
+                  let feedId;
 
-                const items = xmlDom.querySelectorAll('item');
-                items.forEach((item) => {
-                  const postTitle = item.querySelector('title').textContent;
-                  const postDescription = item.querySelector('description').textContent;
-                  const link = item.querySelector('link').textContent;
-                  const postId = uniqueId();
-                  watcher.posts.push({
-                    title: postTitle,
-                    description: postDescription,
-                    link,
-                    id: postId,
-                    feedId,
+                  const allFeedTitles = watcher.feeds.map(({ title }) => title);
+                  if (!allFeedTitles.includes(feedTitle)) {
+                    feedId = uniqueId();
+                    watcher.feeds.push({
+                      title: feedTitle,
+                      description: feedDescription,
+                      id: feedId,
+                    });
+                    watcher.form.valid = true;
+                    watcher.rssLinks.push(rss);
+                  }
+
+                  const items = xmlDom.querySelectorAll('item');
+                  items.forEach((item) => {
+                    const postTitle = item.querySelector('title').textContent;
+                    const postDescription = item.querySelector('description').textContent;
+                    const postLink = item.querySelector('link').textContent;
+                    const postId = uniqueId();
+
+                    const allPostLinks = watcher.posts.map(({ link }) => link);
+                    if (!allPostLinks.includes(postLink)) {
+                      watcher.posts.push({
+                        title: postTitle,
+                        description: postDescription,
+                        link: postLink,
+                        id: postId,
+                        feedId,
+                      });
+                    }
                   });
-                });
-                watcher.rssLinks.push(rssLink);
-                watcher.form.valid = true;
-                setTimeout(checkNewPosts(watcher), 5000);
-              })
-              .then(() => {
-                const postButtons = document.querySelectorAll('.posts button');
-                postButtons.forEach((button) => {
-                  button.addEventListener('click', (el) => {
-                    const buttonId = el.target.dataset.id;
-                    const actualPost = watcher.posts.find(({ id }) => id === buttonId);
-                    watcher.uiState.modal = actualPost;
-                    watcher.uiState.viewedPosts.push(actualPost.id);
+                  setTimeout(requestToRss(rss), 5000);
+                })
+                .then(() => {
+                  const postButtons = document.querySelectorAll('.posts button');
+                  postButtons.forEach((button) => {
+                    button.addEventListener('click', (el) => {
+                      const buttonId = el.target.dataset.id;
+                      const actualPost = watcher.posts.find(({ id }) => id === buttonId);
+                      watcher.uiState.modal = actualPost;
+                      watcher.uiState.viewedPosts.push(actualPost.id);
+                    });
                   });
+                })
+                .catch((err) => {
+                  if (err.request) {
+                    watcher.form.valid = false;
+                    watcher.form.error = 'form.errors.failRequest';
+                    console.error('form.errors.failRequest');
+                    console.error(err);
+                  } else {
+                    watcher.form.valid = false;
+                    watcher.form.error = err.message;
+                    console.error(err);
+                  }
                 });
-              })
-              .catch((err) => {
-                if (err.request) {
-                  watcher.form.valid = false;
-                  watcher.form.error = 'form.errors.failRequest';
-                  console.error('form.errors.failRequest');
-                  console.error(err);
-                } else {
-                  watcher.form.valid = false;
-                  watcher.form.error = err.message;
-                  console.error(err);
-                }
-              });
-            watcher.form.valid = '';
+              watcher.form.valid = '';
+            };
+            requestToRss(rssLink);
           })
           .catch((err) => {
             watcher.form.valid = false;
