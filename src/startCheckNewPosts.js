4c9ee8f9cdd056eaf2ebe526fd88f10a30d1fb58
root <root@DESKTOP-AEVP059>
2023-08-01 23:49:00 +0300

    fix startCheckNewPosts function

--- src/startCheckNewPosts.js
+++ src/startCheckNewPosts.js
@@ -1,15 +1,11 @@
 import requestToRss from './requestToRss.js';
 
 const startCheckNewPosts = (state) => {
-  const promise = Promise.resolve();
-  promise
-    .then(() => {
-      state.rssLinks.forEach((link) => {
-        requestToRss(link, state);
-      });
-    })
-    .then(() => {
-      setTimeout(startCheckNewPosts(state), 5000);
-    });
+  const requests = state.rssLinks.map((link) => Promise.resolve(requestToRss(link, state)));
+  Promise.all(requests).then(() => {
+    setTimeout(() => {
+      startCheckNewPosts(state);
+    }, 5000);
+  });
 };
 export default startCheckNewPosts;
