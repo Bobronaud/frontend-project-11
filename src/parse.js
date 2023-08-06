17971163aa0f9ab415bd58ecde89efec3e3e17fd
root <root@DESKTOP-AEVP059>
2023-08-06 21:40:02 +0300

    make parse.js return feed and posts instead of DOM

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,26 @@
-export default (data, type) => {
-  try {
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(data, type);
-    return dom;
-  } catch (e) {
+import uniqueId from 'lodash/uniqueId.js';
+
+export default (data) => {
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(data, 'text/xml');
+  const errorNode = dom.querySelector('parsererror');
+  if (errorNode) {
     throw new Error('form.errors.failParsing');
   }
+
+  const feed = {
+    title: dom.querySelector('title').textContent,
+    description: dom.querySelector('description').textContent,
+    id: uniqueId(),
+  };
+
+  const items = Array.from(dom.querySelectorAll('item'));
+  const posts = items.map((item) => ({
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    link: item.querySelector('link').textContent,
+    id: uniqueId(),
+    feedId: feed.id,
+  }));
+  return [feed, posts];
 };
